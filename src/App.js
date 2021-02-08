5c2e286d4c9fbbabd24f12962d16810d0f12ecbe
budiyono <budi99.yono@gmail.com>
2021-02-08 14:57:49 +0700

    coba hello world, object, var, css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() { 
+    let obj = {id: 1, nama:"saya"}
+    let hello = "Hello world"
+    const bgColor={
+      backgroundColor:"aqua"
+    }
+    return ( 
+      <>
+      <div>
+        Hello World
+      </div>
+      <div>
+        {hello}
+      </div>
+      <div>
+        {obj.id}
+        {obj.nama}
+      </div>
+      <div style={{backgroundColor:"grey"}}> grey
+      </div>
+      <div style={bgColor}>
+        aqua color
+      </div>
+      </>
+
+    );
+  }
 }
-
+ 
 export default App;
+
